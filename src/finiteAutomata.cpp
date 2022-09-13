ca719ddece40e5736abe5827be2e95edcc51709d
YurafobusX <82900141+YurafobusX@users.noreply.github.com>
2022-09-13 11:28:20 +0500

    Dev (#3)
    0.0.1
    add basic functional

--- src/finiteAutomata.cpp
+++ src/finiteAutomata.cpp
@@ -1,1 +1,33 @@
 #include "finiteAutomata.hpp"
+
+std::ostream& operator<< (std::ostream& stream, const finiteAutomata& r) {
+    stream << r._automataSize << ' ' << r._alphabetSize << '\n';
+    for (auto i : r._nodes) {
+        for (auto j : i.transitions) 
+            stream << j << ' ';
+        stream << '\n';   
+    }
+    return stream;
+};
+
+std::istream& operator>> (std::istream& stream, finiteAutomata& r) {
+    stream >> r._automataSize >> r._alphabetSize;
+    r = finiteAutomata(r._automataSize, r._alphabetSize);
+    for (auto i : r._nodes) {
+        for (auto j : i.transitions) 
+            stream >> j;  
+    }
+    return stream;
+}
+
+void swap(finiteAutomata& l, finiteAutomata& r) {
+    std::swap(l._automataSize, r._automataSize);
+    std::swap(l._alphabetSize, r._alphabetSize);
+    std::swap(l._currentState, r._currentState);
+    std::swap(l._nodes, r._nodes);
+}
+
+finiteAutomata& finiteAutomata::operator=(finiteAutomata r) {
+    swap(*this, r);
+    return *this;
+}
