95c515829a5d087ab0f93d3bdc63c4e6893ee621
YurafobusX <YurafobusX@gmail.com>
2022-10-31 17:09:26 +0500

    add boost::test support

--- tests/main.cpp
+++ tests/main.cpp
@@ -4,9 +4,9 @@
 #include <vector>
 #include <set>
 
-#define BOOST_TEST_MODULE test1
+#define BOOST_TEST_MODULE your_test_module
 #include <boost/test/included/unit_test.hpp>
-#include "finiteAutomata.hpp"
+/*#include "finiteAutomata.hpp"
 #include "uTest.hpp"
 
 class copyConstrT : public uTest {
@@ -92,15 +92,10 @@ class rootT : public uTest {
 		_subTests.emplace_back(new eqTT());
 		_subTests.emplace_back(new ioTT());
 	}
-};
+};*/
 
-int main() {
-	rootT r;
-	std::stringstream s;
-	try {
-		r.run();
-	} catch (std::exception& e) {
-	} 
-	std::cout << '\n';
-	return 0;
-} 
+BOOST_AUTO_TEST_CASE( your_test_case ) {
+    std::vector<int> a{1, 2};
+    std::vector<int> b{1, 2};
+    BOOST_TEST( a == b );
+}
